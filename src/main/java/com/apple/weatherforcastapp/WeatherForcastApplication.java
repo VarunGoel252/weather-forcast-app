c443316ac9ec61f3d55869e87cc9cea15d5a6973
Varun <varungoel252@gmail.com>
2024-03-10 18:52:23 +0530

    Renaming files

--- src/main/java/com/apple/weatherforcastapp/WeatherForcastApplication.java
+++ src/main/java/com/apple/weatherforcastapp/WeatherForcastApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherForcastAppApplication {
+public class WeatherForcastApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeatherForcastAppApplication.class, args);
+		SpringApplication.run(WeatherForcastApplication.class, args);
 	}
 
 }
